1ea5183f1ae0a721a56f5b4b8bbd26f77f08e308
jnsyimh <jnsyimh111@gmail.com>
2021-10-21 13:35:23 +0700

    first commit

---
+++ scr/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/admin.css">
+</head>
+<body>
+    <head>
+        <div class="menu">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="http://localhost:8888/project03/index.php">Home</a></li>
+                    <li><a href="http://localhost:8888/project03/user.php">Users</a></li>
+                    <li><a href="http://localhost:8888/project03/office.php">Office</a></li>
+                    <li><a href="http://localhost:8888/project03/employees.php">Employees</a></li>
+                    <li><a href="http://localhost:8888/project03/logout.php">Logout</a></li>
+                </ul>
+            </div>
+        </div>
+    </head>
